ce1331a716bc3838034c8c5f032cf159c9b3fac3
Ruben Verdin <rverdin@rentsync.com>
2023-05-07 18:11:05 -0400

    Adding icons

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 import { ref } from 'vue'
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-
-
 </script>
 
 <template>
-  <h1>Hello</h1>
+  <main>
+    <h1>Hello</h1>
+  </main>
 </template>
 
 <style lang="scss">
